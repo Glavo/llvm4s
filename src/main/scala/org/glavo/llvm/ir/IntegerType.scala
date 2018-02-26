8be6425a077b940e434f3588b07987254cd8e448
Glavo <zjx001202@126.com>
2018-02-27 07:04:54 +0800

    integer type

--- src/main/scala/org/glavo/llvm/ir/IntegerType.scala
+++ src/main/scala/org/glavo/llvm/ir/IntegerType.scala
@@ -1,11 +1,16 @@
 package org.glavo.llvm.ir
 
-import org.glavo.llvm.Destructor
+import org.glavo.llvm.{Destructor, Unsigned}
 
 class IntegerType private[llvm](handle: Long) extends Type(handle) {
+  def bitWidth: Int@Unsigned = IntegerTypeImpl.getBitWidth(handle)
 
+  def bitMask: Long@Unsigned = IntegerTypeImpl.getBitMask(handle)
+
+  def isPowerOf2ByteWidth: Boolean = IntegerTypeImpl.isPowerOf2ByteWidth(handle)
 }
 
 object IntegerType {
-
+  val MinIntBits: Int = 1
+  val MaxIntBits: Int = (1 << 24) - 1
 }
